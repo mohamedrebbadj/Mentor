811f0fd874895f3031f4bdfe321ea1b4b4764749
mohamed rebbadj <mohamedaminerebbadj@gmail.com>
2021-01-15 11:33:10 +0100

    initial commit

---
+++ dist/js/index.js
@@ -0,0 +1,38 @@
+$(function () {
+  $(".dropdown-menu a.dropdown-toggle").on("click", function (e) {
+    if (!$(this).next().hasClass("show")) {
+      $(this)
+        .parents(".dropdown-menu")
+        .first()
+        .find(".show")
+        .removeClass("show");
+    }
+    var $subMenu = $(this).next(".dropdown-menu");
+    $subMenu.toggleClass("show");
+
+    $(this)
+      .parents("li.nav-item.dropdown.show")
+      .on("hidden.bs.dropdown", function (e) {
+        $(".dropdown__submenu .show").removeClass("show");
+      });
+
+    return false;
+  });
+  let goUp = $(".goUp");
+  function showGoUp() {
+    if ($(window).scrollTop() > 999) {
+        goUp.fadeIn();
+    } else {
+        goUp.fadeOut();
+    }
+  }
+  goUp.on("click", function () {
+
+  });
+  $(window).on("scroll", function () {
+    showGoUp();
+  });
+  // after the page load
+  $(".loader").remove();
+  $("body").css("overflow-y", "auto");
+});
